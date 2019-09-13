b7c5f83cd3ad647354fc3797a211fc15e74b02cc
COE-029 <coe-029@coe-029s-imac.local>
2019-09-13 13:43:11 +0530

    Initial Commit

---
+++ image_slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  image_slider
+//
+//  Created by COE-029 on 13/09/19.
+//  Copyright © 2019 COE-029. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
