37c564a0afd63401000a7188c5b6215b5aa33946
COE-029 <coe-029@coe-029s-imac.local>
2019-09-18 15:36:21 +0530

    image slider

--- image_slider/ViewController.swift
+++ image_slider/ViewController.swift
@@ -9,8 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var ArrayImage = [UIImage]()
+    
+    @IBOutlet weak var ImageView: UIImageView!
+    
+    @IBOutlet weak var SliderOutlet: UISlider!
+    
+    @IBAction func NextButton(_ sender: Any)
+    {
+        SliderOutlet.value += 1
+        ImageView.image = ArrayImage[Int(SliderOutlet.value)]
+        
+    }
+    
+    @IBAction func PrevButton(_ sender: Any)
+    {
+        SliderOutlet.value -= 1
+        ImageView.image = ArrayImage[Int(SliderOutlet.value)]
+    }
+    
+    @IBAction func Slider(_ sender: UISlider)
+    {
+        var value = Int(sender.value)
+        ImageView.image = ArrayImage[value]
+    }
+    
     override func viewDidLoad() {
+        
+        ArrayImage = [#imageLiteral(resourceName: "back1"),#imageLiteral(resourceName: "back2"),#imageLiteral(resourceName: "back3"),#imageLiteral(resourceName: "back4"),#imageLiteral(resourceName: "back5"),#imageLiteral(resourceName: "back6"),#imageLiteral(resourceName: "back7")]
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
